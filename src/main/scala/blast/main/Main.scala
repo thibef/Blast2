62ca3d01803d483a52b5656620d4b9dfa6f271a5
Parsa <oossparsa@gmail.com>
2019-02-18 00:09:55 +0100

    read the data using spark directly --> pay attention to the comments, the original dataset has to be saved in spark ObjectFile format first in order to be read by spark directly.

--- src/main/scala/blast/main/Main.scala
+++ src/main/scala/blast/main/Main.scala
@@ -10,6 +10,8 @@ import DataStructures.Attribute
 import blast.AttributeSchema.AttributeProfile
 import blast.AttributeSchema.AttributeMatchInduction
 import blast.blocking.blocking_main
+import org.spark_project.dmg.pmml.False
+
 import scala.collection.JavaConverters._
 
 
@@ -24,62 +26,62 @@ object Main {
       .getOrCreate()
 
     //**read dataset with spark, should use the old method first to read the data for the first time
-    val dataS1 :RDD[EntityProfile] = spark.sparkContext.objectFile("/home/parsa/Downloads/write_read_test/dplb")
-    val dataS2 :RDD[EntityProfile] = spark.sparkContext.objectFile("/home/parsa/Downloads/write_read_test/acm")
+    val dataS1: RDD[EntityProfile] = spark.sparkContext.objectFile("/home/parsa/Downloads/write_read_test/dplb")
+    val dataS2: RDD[EntityProfile] = spark.sparkContext.objectFile("/home/parsa/Downloads/write_read_test/acm")
 
     //**load the data for the first time using this, and save in spark file object format (basically a bunch of sequenced files)
-//    val dataS1Raw = DatasetReader.readDataset("/home/parsa/Downloads/dataset1_dblp")
-//    val dataS1  : RDD[EntityProfile]= spark.sparkContext.parallelize(dataS1Raw)
-//    val dataS2Raw = DatasetReader.readDataset("/home/parsa/Downloads/dataset2_acm")
-//    val dataS2 : RDD[EntityProfile] = spark.sparkContext.parallelize(dataS2Raw)
+    //    val dataS1Raw = DatasetReader.readDataset("/home/parsa/Downloads/dataset1_dblp")
+    //    val dataS1  : RDD[EntityProfile]= spark.sparkContext.parallelize(dataS1Raw)
+    //    val dataS2Raw = DatasetReader.readDataset("/home/parsa/Downloads/dataset2_acm")
+    //    val dataS2 : RDD[EntityProfile] = spark.sparkContext.parallelize(dataS2Raw)
     //*****************************************************************************************************
     // **save the data from above in spark File object format
-//    dataS1.saveAsObjectFile("/home/parsa/Downloads/write_read_test/dplb")
-//    dataS2.saveAsObjectFile("/home/parsa/Downloads/write_read_test/acm")
+    //    dataS1.saveAsObjectFile("/home/parsa/Downloads/write_read_test/dplb")
+    //    dataS2.saveAsObjectFile("/home/parsa/Downloads/write_read_test/acm")
     //********************************************************************************************************
-    print("DS1 size:",dataS1.count())
-    println("\tDS2 size:",dataS2.count())
+    val size_DS1  = dataS1.count() ; val  size_DS2 = dataS2.count()
 
-
+    println("DS1 size:", size_DS1,"\tDS2 size:", size_DS2)
     println("data loaded")
-    return
-    //Creates AttributeProfile class instances which calculate information regarding attributes
-    val AProfileDS1 =  new AttributeProfile(dataS1)
-    val AProfileDS2 =  new AttributeProfile(dataS2)
 
+    //Creates AttributeProfile class instances which calculate information regarding attributes
+    val AProfileDS1 = new AttributeProfile(dataS1.sample(false, 3.0/size_DS1))
+    val AProfileDS2 = new AttributeProfile(dataS2.sample(false, 3.0/size_DS2))
+    println("sizee after cutting" ,AProfileDS1.getEntityProfiles.count(),AProfileDS2.getEntityProfiles.count())
+    /*
     println("entropies DS1")
     AProfileDS1.getAttributeEntropies.collect.foreach(println)
     println("entropies DS2")
     AProfileDS2.getAttributeEntropies.collect.foreach(println)
-    /*
+
       for ( (attr1name,attr1set) <- AProfileDS1.collect() ) {
       for ( (attr2name,attr2set) <- AProfileDS2.collect() ) {
         println(attr1name +" vs " + attr2name + AttributeProfile.similarity(attr1set, attr2set))
       }
     }
     */
-<<<<<<< HEAD
+
     //val a = new AttributeMatchInduction(AProfileDS1, AProfileDS2)
-=======
-    println("\n###### Match Induction details:")
-    val a = new AttributeMatchInduction(AProfileDS1, AProfileDS2)
->>>>>>> master
+
+    //println("\n###### Match Induction details:")
+    //val a = new AttributeMatchInduction(AProfileDS1, AProfileDS2)
+
     //AttributeMatchInduction(AProfileDS1, AProfileDS2)
     println("\n\nblocking phase details:")
     println("tokens:")
-<<<<<<< HEAD
+
     //AProfileDS1.getAttributeTokens.foreach(println)
     //AProfileDS2.getAttributeTokens.foreach(println)
 
-=======
+
     println("DS1 Tokens:")
     AProfileDS1.getAttributeTokens.foreach(println)
     println("DS2 Tokens:")
     AProfileDS2.getAttributeTokens.foreach(println)
     println("\nentity space tokens:")
-    val blocking = new blocking_main(AProfileDS1,AProfileDS2)
-    blocking.get_combined_tokens().foreach(println)
->>>>>>> master
+    val blocking = new blocking_main(AProfileDS1, AProfileDS2)
+    blocking.get_common_tokens().foreach(print)
+
 
   }
 
